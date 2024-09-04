66195e7099c62570c5f82d900117358096b5d7d2
Mingus0299 <yangming2962@gmail.com>
2024-09-04 00:36:56 -0400

    guessing game and adding loop to ch1-3

--- projects/CH1-3/loops/target/debug/deps/loops-659a9c15d0cc6d03.d
+++
@@ -1,5 +0,0 @@
-/Users/mingus/Documents/yangming2962_github/Untitled/Rust_learning/projects/loops/target/debug/deps/loops-659a9c15d0cc6d03: src/main.rs
-
-/Users/mingus/Documents/yangming2962_github/Untitled/Rust_learning/projects/loops/target/debug/deps/loops-659a9c15d0cc6d03.d: src/main.rs
-
-src/main.rs:
--- projects/CH1-3/loops/target/debug/loops.d
+++
@@ -1,1 +0,0 @@
-/Users/mingus/Documents/yangming2962_github/Untitled/Rust_learning/projects/loops/target/debug/loops: /Users/mingus/Documents/yangming2962_github/Untitled/Rust_learning/projects/loops/src/main.rs
