d18864164546e5cbd59558e8cec35559e994ec60
Mingus0299 <yangming2962@gmail.com>
2024-09-03 23:23:38 -0400

    review functions

---
+++ projects/functions/target/debug/deps/functions-f78fb00d7e51a28e.d
@@ -0,0 +1,5 @@
+/Users/mingus/Documents/yangming2962_github/Untitled/Rust_learning/projects/functions/target/debug/deps/functions-f78fb00d7e51a28e: src/main.rs
+
+/Users/mingus/Documents/yangming2962_github/Untitled/Rust_learning/projects/functions/target/debug/deps/functions-f78fb00d7e51a28e.d: src/main.rs
+
+src/main.rs:
